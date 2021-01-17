8f4dfff1ecce0d253fb80fb8a2ca04e97dbc8a97
Daniilscream <danilvisn@list.ru>
2021-01-17 16:53:34 +0300

    added admin panel and user roles, restricting access to the user editing page

--- src/main/java/ru/Daniilscram/simple_web_application/domain/Role.java
+++ src/main/java/ru/Daniilscram/simple_web_application/domain/Role.java
@@ -3,7 +3,7 @@ package ru.Daniilscram.simple_web_application.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER, ADMIN;
 
     @Override
     public String getAuthority() {
