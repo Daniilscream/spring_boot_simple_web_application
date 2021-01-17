85af93d374d0c936989bffa01f58b911e1754a17
Daniilscream <danilvisn@list.ru>
2021-01-17 14:38:03 +0300

    connected Freemarker

--- src/main/java/ru/Daniilscram/simple_web_application/controller/MainController.java
+++ src/main/java/ru/Daniilscram/simple_web_application/controller/MainController.java
@@ -3,6 +3,7 @@ package ru.Daniilscram.simple_web_application.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,9 +25,15 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String,Object> map){
+    public String main(@RequestParam(required = false, defaultValue = "")String filter, Model model){
         Iterable<Message> messages = messageRepository.findAll();
-        map.put("messages", messages);
+        if(filter!=null&&!filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
@@ -43,15 +50,5 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String,Object> map){
-        Iterable<Message> messages;
-        if(filter!=null&&!filter.isEmpty()) {
-            messages = messageRepository.findByTag(filter);
-        } else {
-            messages = messageRepository.findAll();
-        }
-        map.put("messages", messages);
-        return "main";
-    }
+
 }
