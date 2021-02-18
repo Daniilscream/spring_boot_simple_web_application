5b39ca71bf971eb9be39eeb0f294113215952014
Daniilscream <danilvisn@list.ru>
2021-02-18 23:41:07 +0300

    Добавлен плагин и зависимость FlyWay, миграции БД, добавлено меню профиля для редактирования пароля и email

--- src/main/java/ru/Daniilscram/simple_web_application/controller/UserController.java
+++ src/main/java/ru/Daniilscram/simple_web_application/controller/UserController.java
@@ -2,6 +2,7 @@ package ru.Daniilscram.simple_web_application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,27 +12,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.Daniilscram.simple_web_application.domain.Role;
 import ru.Daniilscram.simple_web_application.domain.User;
-import ru.Daniilscram.simple_web_application.repository.UserRepository;
+import ru.Daniilscram.simple_web_application.service.UserService;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public String userList(Model model){
-        model.addAttribute("users", userRepository.findAll());
+        model.addAttribute("users", userService.findAll());
         return "userList";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("{user}")
     public String userEditForm(@PathVariable User user, Model model){
         model.addAttribute("user", user);
@@ -39,23 +38,34 @@ public class UserController {
         return "userEdit";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping
     public String userSave(
             @RequestParam String username,
             @RequestParam Map<String,String> form,
             @RequestParam("userID") User user){
-        user.setUsername(username);
-        Set<String> roles = Arrays.stream(Role.values())
-                .map(Role::name)
-                .collect(Collectors.toSet());
-        user.getRoles().clear();
-
-        for (String key: form.keySet()) {
-            if(roles.contains(key)){
-                user.getRoles().add(Role.valueOf(key));
-            }
-        }
-        userRepository.save(user);
+        userService.saveUser(user,username,form);
         return "redirect:/user";
+    }
+
+    @GetMapping("profile")
+    public String getProfile(Model model,
+                             @AuthenticationPrincipal User user){
+            model.addAttribute("username",user.getUsername());
+            model.addAttribute("email",user.getEmail());
+
+            return "profile";
+    }
+
+    @PostMapping("profile")
+    public String updateProfile(
+            @AuthenticationPrincipal User user,
+            @RequestParam String password,
+            @RequestParam String email){
+
+        userService.updateProfile(user, password, email);
+
+        return "redirect:/user/profile";
+
     }
 }
--- src/main/java/ru/Daniilscram/simple_web_application/service/UserService.java
+++ src/main/java/ru/Daniilscram/simple_web_application/service/UserService.java
@@ -10,8 +10,13 @@ import ru.Daniilscram.simple_web_application.domain.Role;
 import ru.Daniilscram.simple_web_application.domain.User;
 import ru.Daniilscram.simple_web_application.repository.UserRepository;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -39,6 +44,12 @@ public class UserService implements UserDetailsService {
         user.setActivationCode(UUID.randomUUID().toString());
         userRepository.save(user);
 
+        sendMessage(user);
+
+        return true;
+    }
+
+    public void sendMessage(User user) {
         if(!StringUtils.isEmpty(user.getEmail())){
             String message = String.format(
                     "Здравствуйте, %s! \n" +
@@ -51,8 +62,6 @@ public class UserService implements UserDetailsService {
 
             mailSender.send(user.getEmail(),"Activation code", message);
         }
-
-        return true;
     }
 
     public boolean activateUser(String code) {
@@ -67,5 +76,46 @@ public class UserService implements UserDetailsService {
         userRepository.save(user);
 
         return true;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+        user.getRoles().clear();
+
+        for (String key: form.keySet()) {
+            if(roles.contains(key)){
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepository.save(user);
+    }
+
+    public void updateProfile(User user, String password, String email) {
+        String userEmail = user.getEmail();
+
+        boolean isEmailChanged = (email != null && !email.equals(userEmail) ||
+                (userEmail != null && !userEmail.equals(email)));
+
+        if(isEmailChanged){
+            user.setEmail(email);
+            if(!StringUtils.isEmpty(email)){
+                user.setActivationCode(UUID.randomUUID().toString());
+            }
+        }
+        if (StringUtils.isEmpty(password)){
+            user.setPassword(password);
+        }
+        userRepository.save(user);
+
+        if(isEmailChanged) {
+            sendMessage(user);
+        }
     }
 }
